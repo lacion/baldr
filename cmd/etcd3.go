aec551014f2e4b28df44b7b6175e288969b28420
Luis Morales <luismmorales@gmail.com>
2016-07-21 16:35:10 +0200

    fixed typo in etcd3 flag

--- cmd/etcd3.go
+++ cmd/etcd3.go
@@ -67,5 +67,5 @@ baldr etcd3 -e etcd1:2379,etcd2:2379,etcd3:2379`,
 func init() {
 	RootCmd.AddCommand(etcd3Cmd)
 
-	etcd3Cmd.Flags().StringVarP(&mongoDB, "endpoints", "e", "", "etcd3 instances to connect to")
+	etcd3Cmd.Flags().StringVarP(&endpoints, "endpoints", "e", "", "etcd3 instances to connect to")
 }
